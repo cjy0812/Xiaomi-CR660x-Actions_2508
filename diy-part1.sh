07565535f7af5f3f5da2d331ffc9b98b589f8e45
Canghaizhifeng <121001029+cjy0812@users.noreply.github.com>
2025-09-12 09:50:36 +0800

    更新 diy-part1.sh
    预期不符，改回默认配置

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,8 +18,3 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
-# Add some App
-echo 'src-git theme-design https://github.com/0x676e67/luci-theme-design' >> feeds.conf.default
-echo 'src-git wizard https://github.com/sirpdboy/luci-app-wizard' >> feeds.conf.default
-echo 'src-git netspeedtest https://github.com/sirpdboy/luci-app-netspeedtest' >> feeds.conf.default
-echo 'src-git taskplan  https://github.com/sirpdboy/luci-app-taskplan' >> feeds.conf.default
