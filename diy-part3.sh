74f7fb601b44aefd44fb280647dc5b7fde65a9ef
Canghaizhifeng <121001029+cjy0812@users.noreply.github.com>
2025-09-12 09:37:18 +0800

    创建 diy-part3.sh
    test

---
+++ diy-part3.sh
@@ -0,0 +1,5 @@
+# Add some App
+echo 'src-git theme-design https://github.com/0x676e67/luci-theme-design' >> feeds.conf.default
+echo 'src-git wizard https://github.com/sirpdboy/luci-app-wizard' >> feeds.conf.default
+echo 'src-git netspeedtest https://github.com/sirpdboy/luci-app-netspeedtest' >> feeds.conf.default
+echo 'src-git taskplan  https://github.com/sirpdboy/luci-app-taskplan' >> feeds.conf.default
