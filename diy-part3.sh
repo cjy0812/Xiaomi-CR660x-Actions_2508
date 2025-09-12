0cad38af55c2eedbe2b94792c2839fb1452926ad
Canghaizhifeng <121001029+cjy0812@users.noreply.github.com>
2025-09-12 15:59:42 +0800

    更新 diy-part3.sh
    add apps

--- diy-part3.sh
+++ diy-part3.sh
@@ -3,3 +3,4 @@ git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-des
 git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
 git clone https://github.com/sirpdboy/luci-app-netspeedtest package/netspeedtest
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+git clone https://github.com/cjy0812/luci-app-adguardhome
