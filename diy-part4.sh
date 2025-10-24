7a8d958085bf87e5daa0dc11e8d66af0906aaf53
Canghaizhifeng <121001029+cjy0812@users.noreply.github.com>
2025-10-24 18:47:02 +0800

    Update diy-part4.sh
    为op加入自动开启无线第一次刷入

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,3 +1,23 @@
 #删除旧feeds冲突主题
 rm -rf ./feeds/luci/themes/luci-theme-argon
 rm -rf ./feeds/luci/themes/luci-theme-design
+
+# 创建默认开启无线脚本
+cat > package/base-files/files/etc/uci-defaults/99-auto-enable-wifi << 'EOF'
+#!/bin/sh
+# 默认开启所有无线设备
+
+. /lib/functions.sh
+config_load wireless
+
+wifi_enable_device() {
+  local cfg="$1"
+  uci set wireless.$cfg.disabled='0'
+}
+
+config_foreach wifi_enable_device wifi-device
+uci commit wireless
+EOF
+
+# 设置可执行权限
+chmod +x package/base-files/files/etc/uci-defaults/99-auto-enable-wifi
